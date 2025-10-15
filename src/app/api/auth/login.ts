edacd28837ad9bfeea4162d8d230383cc5e7b12b
tanahiro2010 <herentongkegu087@gmail.com>
2025-10-15 18:50:04 +0900

    Add: login api

---
+++ src/app/api/auth/login.ts
@@ -0,0 +1,28 @@
+"use server";
+import { successResponse, notFoundResponse, errorResponse } from "@/lib/api/response";
+import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { User } from "@/lib/service/user";
+
+export async function POST(req: NextRequest) {
+    const [{ email, password }, store] = await Promise.all([req.json(), cookies()]);
+
+    const user = await User.get({ email });
+    if (!user) {
+        return notFoundResponse("User not found");
+    }
+
+    const token = await user.login(password);
+    if (!token) {
+        return errorResponse("Invalid password", { status: 401 });
+    }
+
+    store.set("s-token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60, // 7 days
+    });
+
+    return successResponse({ token });
+}
