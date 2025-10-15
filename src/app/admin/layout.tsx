1146584e680eb0dd28b108dce38fead7be2d0965
tanahiro2010 <herentongkegu087@gmail.com>
2025-10-15 22:53:41 +0900

    Fix: permission and redirect url

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -11,11 +11,6 @@ export default async function AdminLayout({
 
   // ログインしていない場合
   if (!user) {
-    redirect("/login?redirect=/admin");
-  }
-
-  // 管理者権限がない場合
-  if (user.role !== "ADMIN") {
     redirect("/403");
   }
 
