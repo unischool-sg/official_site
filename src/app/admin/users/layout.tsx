a63d4224c1f12c8babf57d009417b0e89f67a3df
tanahiro2010 <herentongkegu087@gmail.com>
2025-10-16 13:07:39 +0900

    Add: auth

---
+++ src/app/admin/users/layout.tsx
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation";
+import { User } from "@/lib/service/user";
+
+export default async function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // サーバーサイドで詳細な認証チェック
+  const user = await User.current(true); // プロフィール込みで取得
+  if (!user || user.role !== "ADMIN") {
+    redirect("/403");
+  }
+
+  return <>{ children }</>;
+}
