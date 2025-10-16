aa72825fc783cd76c864b1f1a81d165820253894
tanahiro2010 <herentongkegu087@gmail.com>
2025-10-16 13:05:03 +0900

    Edit: add send function

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,12 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-export { resend };
+async function send(to: string, subject: string, html: string) {
+    return await resend.emails.send({
+        from: process.env.RESEND_FROM_EMAIL || "",
+        to,
+        subject,
+        html,
+    });
+}
+export { resend, send };
