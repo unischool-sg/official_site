7ea02054df73c1c4d71fcefaa8ebe199954d0f75
tanahiro2010 <herentongkegu087@gmail.com>
2025-10-16 13:07:23 +0900

    Edit: layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,8 @@
+import AdminSidebar from "@/components/layout/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { redirect } from "next/navigation";
-import { session } from "../layout";
+import { User } from "@/lib/service/user";
+
 
 export default async function AdminLayout({
   children,
@@ -7,10 +10,17 @@ export default async function AdminLayout({
   children: React.ReactNode;
 }) {
   // サーバーサイドで詳細な認証チェック
-  const user = session.get("user");
+  const user = await User.current(true); // プロフィール込みで取得
   if (!user) {
     redirect("/403");
   }
 
-  return <>{children}</>;
+  return (
+    <SidebarProvider>
+      <div className="flex w-full min-h-screen">
+        <AdminSidebar user={user} />
+        <main className="flex-1">{children}</main>
+      </div>
+    </SidebarProvider>
+  );
 }
