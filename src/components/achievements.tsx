e12e5ac21cd00a22003cb30cea76a6388f3ae378
tanahiro2010 <herentongkegu087@gmail.com>
2025-10-19 19:35:03 +0900

    Edit: archive link

--- src/components/achievements.tsx
+++ src/components/achievements.tsx
@@ -2,20 +2,20 @@ import { Container } from "@/components/ui/container";
 import { BlurFade } from "@/components/ui/blur-fade";
 
 export default function VideoCard({
-     youtubeId,
      title,
      description,
+     driveId,
 }: {
-     youtubeId: string;
      title: string;
      description: string;
+     driveId: string;
 }) {
      return (
           <div className="flex flex-col">
                <div className="aspect-video mb-3">
                     <iframe
                          className="w-full h-full rounded"
-                         src={`https://www.youtube.com/embed/${youtubeId}`}
+                         src={`https://drive.google.com/file/d/${driveId}/preview`}
                          title={title}
                          allowFullScreen
                     />
@@ -26,21 +26,29 @@ export default function VideoCard({
      );
 }
 
-const videos = [
+interface Video {
+     driveId: string;
+     title: string;
+     description: string;
+}
+
+const videos: Video[] = [
      {
-          youtubeId: "dQw4w9WgXcQ",
-          title: "動画タイトル1",
-          description: "これは説明文1です。",
+          driveId: "1I6UiYdQghoNLbf0Vs7GNLCsw7GQdOu-S",
+          title: "三田学園中学校陸上部紹介",
+          description: "三田学園中学校の陸上部を紹介しています。",
      },
      {
-          youtubeId: "dQw4w9WgXcQ",
-          title: "動画タイトル2",
-          description: "これは説明文2です。",
+          // https://drive.google.com/file/d/1-hn-zpipT22EiQerrVAoKYcjxIhFXLDr/view?usp=drive_link
+          driveId: "1-hn-zpipT22EiQerrVAoKYcjxIhFXLDr",
+          title: "ラーニングコンパス紹介動画",
+          description: "三田学園で行われている、教育の方向性を生徒や教員主体で考え、学びの幅を広げていく、ラーニングコンパスという活動の紹介動画です。",
      },
      {
-          youtubeId: "dQw4w9WgXcQ",
-          title: "動画タイトル3",
-          description: "これは説明文3です。",
+          // https://drive.google.com/file/d/1S5T54uS6-FqfpfPgGdjg6ZTuRZ2_qHlP/view?usp=drive_link
+          driveId: "1S5T54uS6-FqfpfPgGdjg6ZTuRZ2_qHlP",
+          title: "三田学園Map紹介",
+          description: "三田学園の校内施設を紹介しています。",
      },
 ];
 
@@ -62,7 +70,7 @@ export function Achievements() {
                     <BlurFade delay={0.5} inView>
                          <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
                               {videos.map((v) => (
-                                   <VideoCard key={v.youtubeId} {...v} />
+                                   <VideoCard key={v.driveId} {...v} />
                               ))}
                          </div>
                     </BlurFade>
