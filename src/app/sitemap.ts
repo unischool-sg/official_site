baa10e4e03d89b7dc5307d8e09b225fe166c4f8b
tanahiro2010 <herentongkegu087@gmail.com>
2026-04-06 17:53:20 +0900

    fix: update public user mapping to use profiles for sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -30,6 +30,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         };
     });
     const publicUsers = users.filter((user) => user.profile?.isPublic);
+    const publicProfiles: Profile[] = publicUsers.map((user) => user.profile!) as Profile[];
     const defaultSitemap: MetadataRoute.Sitemap = [
         {
             url: baseUrl + "/",
@@ -48,9 +49,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return [
         ...defaultSitemap,
         ...blogs.map(({ url, lastModified }) => ({ url, lastModified })),
-        ...publicUsers.map((member) => ({
-            url: `${baseUrl}/members/${member.id}`,
-            lastModified: member.updatedAt,
+        ...publicProfiles.map((profile: Profile) => ({
+            url: `${baseUrl}/members/${profile.userId}`,
+            lastModified: profile.updatedAt,
         })),
     ];
 }
