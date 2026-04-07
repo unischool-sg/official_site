12ec2cf4d31ccacccf60682fec1387788c5f9f53
tanahiro2010 <herentongkegu087@gmail.com>
2026-04-07 10:40:59 +0900

    fix: update metadata generation to include keywords from user names and layout

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,12 +5,17 @@ import { About } from "@/components/about";
 import { Members } from "@/components/members";
 import { Achievements } from "@/components/achievements";
 import { User } from "@/lib/service/user";
+import { metadata } from "../layout";
 
 export async function generateMetadata(): Promise<Metadata> {
     const users = await User.all(false);
     const names = users.map((user) => user.name);
+    const keywords = new Array();
+    keywords.push(...names);
+    keywords.push(...metadata.keywords!);
+    
     return {
-        keywords: [...names]
+        keywords
     }
 }
 
